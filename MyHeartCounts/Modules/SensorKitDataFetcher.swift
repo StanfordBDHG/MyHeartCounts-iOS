fc1afc4879efbdbbed18bb8d4e01fa18426832da
Lukas Kollmer <hey@lukaskollmer.de>
2025-09-13 11:21:51 +0200

    SensorKitDataFetcher: use a for loop, parallelize a bit

--- MyHeartCounts/Modules/SensorKitDataFetcher.swift
+++ MyHeartCounts/Modules/SensorKitDataFetcher.swift
@@ -6,13 +6,15 @@
 // SPDX-License-Identifier: MIT
 //
 
+// swiftlint:disable file_types_order
+
 import OSLog
 import Spezi
 import SpeziFoundation
 import SpeziSensorKit
 
 
-final class SensorKitDataFetcher: Module {
+final class SensorKitDataFetcher: Module, @unchecked Sendable {
     // swiftlint:disable attributes
     @StandardActor private var standard: MyHeartCountsStandard
     @Dependency(SensorKit.self) private var sensorKit
@@ -28,31 +30,33 @@ final class SensorKitDataFetcher: Module {
     
     @concurrent
     private func doFetch() async {
-        func imp<Sample>(_ sensor: Sensor<Sample>) async where Sample.SafeRepresentation: HealthObservation {
-            guard sensorKit.authorizationStatus(for: sensor) == .authorized else {
-                logger.notice("Skipping Sensor '\(sensor.displayName)' bc it's not authorized.")
-                return
-            }
-            do {
-                logger.notice("will fetch new samples for Sensor '\(sensor.displayName)'")
-                for try await batch in try await sensorKit.fetchAnchored(sensor) {
-                    try await standard.uploadHealthObservations(batch)
-                }
-            } catch {
-                logger.error("Failed to fetch & upload data for Sensor '\(sensor.displayName)': \(error)")
-            }
-        }
         await withDiscardingTaskGroup { taskGroup in
-            taskGroup.addTask {
-                await imp(.onWrist)
+            for sensor in SensorKit.mhcSensors {
+                taskGroup.addTask {
+                    await self.fetchAndUploadAnchored(sensor)
+                }
             }
-            taskGroup.addTask {
-                await imp(.ecg)
-            }
-            for try await _ in taskGroup {} // not sure if this is actually needed...
         }
     }
     
+    
+    /// Fetches all new SensorKit samples for the specified sensor (relative to the last time the function was called for the sensor), and uploads them all into the Firestore.
+    @concurrent
+    private func fetchAndUploadAnchored<Sample>(_ sensor: some MHCUploadableSensor<Sample>) async {
+        let sensor = Sensor(sensor)
+        guard sensorKit.authorizationStatus(for: sensor) == .authorized else {
+            logger.notice("Skipping Sensor '\(sensor.displayName)' bc it's not authorized.")
+            return
+        }
+        do {
+            logger.notice("will fetch new samples for Sensor '\(sensor.displayName)'")
+            for try await batch in try await sensorKit.fetchAnchored(sensor) {
+                try await standard.uploadHealthObservations(batch)
+            }
+        } catch {
+            logger.error("Failed to fetch & upload data for Sensor '\(sensor.displayName)': \(error)")
+        }
+    }
     
     /// Fetches all SensorKit samples for the specified sensor, and uploads them all into the Firestore.
     ///
@@ -74,8 +78,15 @@ final class SensorKitDataFetcher: Module {
 }
 
 
+// MARK: Sensors
+
+protocol MHCUploadableSensor<Sample>: AnySensor where Sample.SafeRepresentation: HealthObservation {}
+
+extension Sensor: MHCUploadableSensor where Sample.SafeRepresentation: HealthObservation {}
+
+
 extension SensorKit {
-    static let mhcSensors: [any AnySensor] = [
+    static let mhcSensors: [any MHCUploadableSensor] = [
         Sensor.onWrist,
         Sensor.ecg
     ]
