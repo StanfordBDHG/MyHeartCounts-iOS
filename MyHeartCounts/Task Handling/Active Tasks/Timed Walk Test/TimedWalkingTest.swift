058ca8e45f09461809eb60fe318e89cb3546432c
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2025-10-12 05:13:38 -0700

    Update Timed Walking Test UX (#62)
    # Update Timed Walking Test UX
    
    ## :recycle: Current situation & Problem
    - Improves the layout for the timed walking text
    
    ## :gear: Release Notes
    - Update Timed Walking Test UX
    
    ### Code of Conduct & Contributing Guidelines
    By creating and submitting this pull request, you agree to follow our
    [Code of
    Conduct](https://github.com/StanfordBDHG/.github/blob/main/CODE_OF_CONDUCT.md)
    and [Contributing
    Guidelines](https://github.com/StanfordBDHG/.github/blob/main/CONTRIBUTING.md):
    - [x] I agree to follow the [Code of
    Conduct](https://github.com/StanfordBDHG/.github/blob/main/CODE_OF_CONDUCT.md)
    and [Contributing
    Guidelines](https://github.com/StanfordBDHG/.github/blob/main/CONTRIBUTING.md).
    
    ---------
    
    Co-authored-by: Lukas Kollmer <hey@lukaskollmer.de>

--- MyHeartCounts/Task Handling/Active Tasks/Timed Walk Test/TimedWalkingTest.swift
+++ MyHeartCounts/Task Handling/Active Tasks/Timed Walk Test/TimedWalkingTest.swift
@@ -113,11 +113,11 @@ final class TimedWalkingTest: Module, EnvironmentAccessible, Sendable {
             throw .unableToStart(.missingSensorPermissions)
         }
         do {
-            try await watchManager.startWorkoutOnWatch(for: test.kind)
+            try await watchManager.startWorkoutOnWatch(for: test)
             logger.notice("Successfully launched watch app")
         } catch {
-            // we still continue if the watch workout failed.
-            logger.notice("Failed to start watch workout: \(error)")
+            // we still continue if the watch app failed to launch.
+            logger.notice("Failed to launch watch app: \(error)")
         }
         let startInstant = ContinuousClock.Instant.now
         let session = ActiveSession(test: test, startDate: .now)
