031b49d438fa893c95bddaac6c96935ede33f873
Lukas Kollmer <hey@lukaskollmer.de>
2025-10-19 19:28:46 +0200

    ^^

--- MyHeartCountsUITests/TaskHandlingTests.swift
+++ MyHeartCountsUITests/TaskHandlingTests.swift
@@ -87,12 +87,19 @@ final class TaskHandlingTests: MHCTestCase, @unchecked Sendable {
         }
         let numTestsBefore = numTests
         
+        let expectedDistanceText = switch Locale.current.measurementSystem {
+        case .us:
+            "2,762 ft"
+        default:
+            "842 m"
+        }
+        
         if case let button = app.buttons.element(matching: testCellPred),
            button.waitForExistence(timeout: 2) {
             button.firstMatch.tap()
             XCTAssert(app.navigationBars["0.1-Minute Walk Test"].waitForExistence(timeout: 2))
             XCTAssert(app.staticTexts["Number of Steps, 624"].waitForExistence(timeout: 2))
-            XCTAssert(app.staticTexts["Distance, 842 m"].waitForExistence(timeout: 2))
+            XCTAssert(app.staticTexts["Distance, \(expectedDistanceText)"].waitForExistence(timeout: 2))
             app.navigationBars["0.1-Minute Walk Test"].buttons["BackButton"].tap()
         }
         
@@ -102,7 +109,7 @@ final class TaskHandlingTests: MHCTestCase, @unchecked Sendable {
         handleMotionAndFitnessAccessPrompt(timeout: .seconds(2))
         XCTAssert(app.staticTexts["Test Complete"].waitForExistence(timeout: 10))
         XCTAssert(app.staticTexts["Steps, 624"].exists)
-        XCTAssert(app.staticTexts["Distance (m), 842"].exists)
+        XCTAssert(app.staticTexts["Distance, \(expectedDistanceText)"].exists)
         print(app.debugDescription)
         app.otherElements["MHC.TimedWalkTestView"].navigationBars.buttons["Close"].tap()
         let numTestsAfter = numTests
