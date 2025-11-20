db4703797c9426690cc5d1de564a0f3cfed58cef
Lukas Kollmer <hey@lukaskollmer.de>
2025-11-20 13:40:24 +0100

    try to fix the bug where you'd sometimes end up in an invalid state if you get logged out but also aren't unenrolled
    not sure what's causing this actually

--- MyHeartCounts/MyHeartCountsStandard.swift
+++ MyHeartCounts/MyHeartCountsStandard.swift
@@ -6,6 +6,7 @@
 // SPDX-License-Identifier: MIT
 //
 
+@preconcurrency import FirebaseAuth
 @preconcurrency import FirebaseFirestore
 @preconcurrency import FirebaseStorage
 import HealthKitOnFHIR
@@ -53,7 +54,22 @@ actor MyHeartCountsStandard: Standard, EnvironmentAccessible, AccountNotifyConst
     @MainActor
     func configure() {
         _Concurrency.Task {
+            await handleIsLoggedOut()
             await updateStudyDefinition()
+        }
+    }
+    
+    @MainActor
+    private func handleIsLoggedOut() async {
+        let isLoggedIn1 = Auth.auth().currentUser != nil
+        let isLoggedIn2 = await account?.signedIn ?? false
+        if !isLoggedIn1 && !isLoggedIn2 {
+            // both firebase and SpeziAccount tell us that there currently is no logged-in user.
+            do {
+                try await performLogoutCleanup()
+            } catch {
+                await logger.error("\(#function): \(error)")
+            }
         }
     }
     
@@ -103,53 +119,62 @@ actor MyHeartCountsStandard: Standard, EnvironmentAccessible, AccountNotifyConst
     // MARK: Account Stuff
     
     func respondToEvent(_ event: AccountNotifications.Event) async {
+        print("ACCOUNT EVENT \(event)")
         let logger = logger
         switch event {
         case .deletingAccount:
             logger.notice("account is being deleted")
         case .disassociatingAccount:
             logger.notice("account is disassociating")
-            // upon logging out, we want to throw the user back to the onboarding.
-            // note that the onboarding flow, in this context, won't work 100% identical to when you've just launched the app in a non-logged-in state,
-            // since the Firebase SDK and all related Spezi modules will still be loaded.
-            // we could look into using the `FirebaseApp.deleteApp(_:)` API in combination with attempting to unload the related Spezi modules, but that
-            // would be anything but trivial.
-            // if the user wants to switch to a different region, the easiest approach currently is to just kill and relaunch the app.
-            try? await managedFileUpload.clearPendingUploads()
-            try? await historicalUploadManager.fullyResetSession(restart: false)
-            sensorKitFetcher.resetAllQueryAnchors()
-            let studyManager = studyManager
-            _ = await _Concurrency.Task { @MainActor in
-                // this works bc we only ever enroll into the MHC study.
-                guard let studyManager, let enrollment = studyManager.studyEnrollments.first else {
-                    return
-                }
-                logger.notice("unenrolling from study.")
-                do {
-                    try await studyManager.unenroll(from: enrollment)
-                } catch {
-                    logger.error("Error unenrolling from study: \(error)")
-                }
-            }.result
-            _Concurrency.Task {
-                guard /*!ProcessInfo.isBeingUITested,*/ await !setupTestEnvironment.isInSetup else {
-                    // ^we potentially log out and in as part of the test env setup; we want to skip this
-                    return
-                }
-                // it seems that the fact that the account sheet typically is still presented while logging out causes issues with us setting the
-                // `onboardingFlowComplete` UserDefaults key being set to true (likely bc the other sheet still being presented prevents SwiftUI from presenting the
-                // onboarding sheet, thereby causing it to set the UserDefaults key (which, via a Binding, is used as the onboarding sheet's `isPresented` value)
-                // back to false.
-                // We try to work around this by waiting a bit, to give the account sheet a chance to dismiss itself.
-                try await _Concurrency.Task.sleep(for: .seconds(2))
-                logger.notice("Triggering Onboarding Flow")
-                LocalPreferencesStore.standard[.onboardingFlowComplete] = false
-            }
+            try? await performLogoutCleanup()
         case .associatedAccount(let details):
             logger.notice("account was associated (account id: \(details.accountId))")
             try? await timeZoneTracking?.updateTimeZoneInfo()
         case .detailsChanged:
             break
+        }
+    }
+    
+    
+    @MainActor
+    private func performLogoutCleanup() async throws {
+        // upon logging out, we want to throw the user back to the onboarding.
+        // note that the onboarding flow, in this context, won't work 100% identical to when you've just launched the app in a non-logged-in state,
+        // since the Firebase SDK and all related Spezi modules will still be loaded.
+        // we could look into using the `FirebaseApp.deleteApp(_:)` API in combination with attempting to unload the related Spezi modules, but that
+        // would be anything but trivial.
+        // if the user wants to switch to a different region, the easiest approach currently is to just kill and relaunch the app.
+        try? await managedFileUpload.clearPendingUploads()
+        try? await historicalUploadManager.fullyResetSession(restart: false)
+        await sensorKitFetcher.resetAllQueryAnchors()
+        let studyManager = await studyManager
+        _ = await _Concurrency.Task { @MainActor in
+            guard let studyManager else {
+                return
+            }
+            await logger.notice("unenrolling from study.")
+            // there should only ever be one enrollment (the MHC one)
+            for enrollment in studyManager.studyEnrollments {
+                do {
+                    try await studyManager.unenroll(from: enrollment)
+                } catch {
+                    await logger.error("Error unenrolling from study: \(error)")
+                }
+            }
+        }.result
+        _Concurrency.Task {
+            guard /*!ProcessInfo.isBeingUITested,*/ await !setupTestEnvironment.isInSetup else {
+                // ^we potentially log out and in as part of the test env setup; we want to skip this
+                return
+            }
+            // it seems that the fact that the account sheet typically is still presented while logging out causes issues with us setting the
+            // `onboardingFlowComplete` UserDefaults key being set to true (likely bc the other sheet still being presented prevents SwiftUI from presenting the
+            // onboarding sheet, thereby causing it to set the UserDefaults key (which, via a Binding, is used as the onboarding sheet's `isPresented` value)
+            // back to false.
+            // We try to work around this by waiting a bit, to give the account sheet a chance to dismiss itself.
+            try await _Concurrency.Task.sleep(for: .seconds(2))
+            await logger.notice("Triggering Onboarding Flow")
+            LocalPreferencesStore.standard[.onboardingFlowComplete] = false
         }
     }
 }
