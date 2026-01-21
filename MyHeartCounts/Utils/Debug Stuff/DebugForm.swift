a72ad19d2b735e40c5e44595e010753f19f315c4
Lukas Kollmer <hey@lukaskollmer.de>
2026-01-21 16:33:56 +0100

    x

--- MyHeartCounts/Utils/Debug Stuff/DebugForm.swift
+++ MyHeartCounts/Utils/Debug Stuff/DebugForm.swift
@@ -98,7 +98,7 @@ private struct DebugFormImpl: View {
     
     private var answerQuestionnaireRow: some View {
         Menu {
-            let options = ["HeartRisk", "Diet", "NicotineExposure"]
+            let options = ["HeartRisk", "Diet", "NicotineExposure", "WHO5"]
             ForEach(options, id: \.self) { option in
                 Button(option) {
                     let fileRef = StudyBundle.FileReference(category: .questionnaire, filename: option, fileExtension: "json")
