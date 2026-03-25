6190a648e43f76288eaaca0294744b63a45e5e73
Paul Goldschmidt <mail@paul-goldschmidt.de>
2026-03-25 10:02:26 +0100

    add function to track language

---
+++ MyHeartCounts/Modules/LanguageTracking.swift
@@ -0,0 +1,33 @@
+//
+// This source file is part of the My Heart Counts iOS application based on the Stanford Spezi Template Application project
+//
+// SPDX-FileCopyrightText: 2025 Stanford University
+//
+// SPDX-License-Identifier: MIT
+//
+
+import Foundation
+import Spezi
+import SpeziAccount
+
+
+final class LanguageTracking: Module, @unchecked Sendable {
+    @Dependency(Account.self)
+    private var account: Account?
+
+    func configure() {
+        Task {
+            try? await updateLanguageInfo()
+        }
+    }
+
+    func updateLanguageInfo() async throws {
+        guard let account else {
+            return
+        }
+        let languageCode = Locale.current.language.languageCode?.identifier ?? "en"
+        var newDetails = AccountDetails()
+        newDetails.language = languageCode
+        try await account.accountService.updateAccountDetails(.init(modifiedDetails: newDetails))
+    }
+}
