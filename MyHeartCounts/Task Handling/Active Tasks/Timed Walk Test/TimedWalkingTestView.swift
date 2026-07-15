7979461dc4858f0688d1e6ac0bafc31c5851d289
Lukas Kollmer <hey@lukaskollmer.de>
2026-07-15 19:06:18 +0200

    vo2 max

--- MyHeartCounts/Task Handling/Active Tasks/Timed Walk Test/TimedWalkingTestView.swift
+++ MyHeartCounts/Task Handling/Active Tasks/Timed Walk Test/TimedWalkingTestView.swift
@@ -204,6 +204,14 @@ private struct TimedWalkingTestView: View {
                     value: Measurement<UnitLength>(value: result.distanceCovered, unit: .meters),
                     format: .measurement(width: .abbreviated)
                 )
+                LabeledContent(
+                    "VO2MAX_ESTIMATE_LABEL",
+                    value: Measurement<UnitOxygenConsumption>(
+                        value: (result.distanceCovered - 504.9) / 44.73,
+                        unit: .mlPerKgPerMin
+                    ),
+                    format: .measurement(width: .abbreviated, usage: .asProvided, numberFormatStyle: .number.precision(.fractionLength(...1)))
+                )
             }
         }
     }
