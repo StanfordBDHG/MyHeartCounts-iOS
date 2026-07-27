77230d53b510cb60bfbd39c9752201ff4305e69f
Lukas Kollmer <hey@lukaskollmer.de>
2026-07-27 10:31:29 +0200

    hmmm

--- MyHeartCountsShared/Package.swift
+++ MyHeartCountsShared/Package.swift
@@ -11,7 +11,7 @@ import PackageDescription
 
 
 var packageDeps: [Package.Dependency] = [
-    .package(url: "https://github.com/SchmiedmayerLab/Spezi.git", branch: "lukas/FHIRModels"),
+    .package(url: "https://github.com/SchmiedmayerLab/Spezi.git", revision: "a418f489eebc52713e8308c845540af78b6aa107"),
     .package(url: "https://github.com/apple/swift-nio.git", from: "2.93.0"),
     .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.7.0")
 ]
