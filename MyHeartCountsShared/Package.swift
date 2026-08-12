58be2091b5f35f56bdecd3ff456bda8e129b1230
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2026-08-12 02:41:39 -0700

    Pin the Spezi Feature Revision Consistently

--- MyHeartCountsShared/Package.swift
+++ MyHeartCountsShared/Package.swift
@@ -11,8 +11,8 @@ import PackageDescription
 
 
 var packageDeps: [Package.Dependency] = [
-    // Tracks the study-bundle feature branch until the next Spezi release.
-    .package(url: "https://github.com/SchmiedmayerLab/Spezi.git", branch: "feature/study-bundle-archive"),
+    // Pins the study-bundle feature revision until the next Spezi release.
+    .package(url: "https://github.com/SchmiedmayerLab/Spezi.git", revision: "4a1240eff42fca1c94a5f495cfeb9b7e83db26ed"),
     .package(url: "https://github.com/apple/swift-nio.git", from: "2.93.0"),
     .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.7.0")
 ]
